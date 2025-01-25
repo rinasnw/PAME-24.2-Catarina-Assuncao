bef64d6c92dea6b7271a72006ed0f74947e48451
rinasnw <rinasniw.20221@poli.ufrj.br>
2025-01-25 20:36:10 -0300

    adicionei uma pasta para algumas funcoes

---
+++ funcoes.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const readline = require('readline-sync');
+
+function exibirMenuFuncionario() {
+    console.log("\n=== MENU FUNCIONÁRIO ===");
+    console.log("4. Ver Meus Dados");
+    console.log("5. Ver Lista de Reservas");
+    console.log("6. Ver Lista de Quartos");
+    console.log("7. Ver Lista de Clientes");
+    console.log("8. Mudar Status da Reserva");
+    console.log("9. Adicionar Quarto");
+    console.log("10. Sair");
+}
+
+module.exports = {
+    exibirMenuFuncionario: exibirMenuFuncionario,
+};
